ecfdba379bc16e6447619ec1f9e0e3253ea894ae
Deric Cheung <cheung.deric@gmail.com>
2021-01-16 14:27:31 -0700

    Remove setup.py. Add dotenv

--- giraffe/head.py
+++ giraffe/head.py
@@ -1,14 +1,13 @@
 import discord
 import os
 
-settings = {}
-with open('/app/settings.cfg', 'r') as configFile:
-    for line in configFile:
-        setting = line.split('=')
-        settings[setting[0]] = setting[1].rstrip()
+from dotenv import load_dotenv
+load_dotenv() # Load environment variables from .env file (located at giraffe/.env)
+# .env contents:
+#   DISCORD_TOKEN=your discord token
 
 client = discord.Client()
-PREFIX = settings.get('COMMAND_PREFIX')
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -18,9 +17,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(PREFIX + 'hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$test'):
+        await message.channel.send('Test complete!')
 
-
-
-client.run(settings.get('DISCORD_TOKEN'))
+client.run(os.getenv('DISCORD_TOKEN'))
